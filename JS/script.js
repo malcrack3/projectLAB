c82ccc6b805b7be7546c7952ab3bf2420a5f5cd8
malcrack3 <pricillia.at@gmail.com>
2025-05-19 01:27:55 +0700

    update done home dan register

--- JS/script.js
+++ JS/script.js
@@ -28,3 +28,58 @@ $(document).ready(function () {
         tampilkanGambar();
     });
 });
+
+
+//=================================================================================
+
+// Register Page
+const isAlphanumeric = (password) => {
+    let hasLetter = false;
+    let hasNumber = false;
+    for(let i = 0; i < password.length; i++){
+
+        if((char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z')) {
+            hasLetter = true;
+        }
+
+
+        if(char >= '0' && char <= '9') {
+            hasNumber = true;
+        }
+    }
+
+    return hasLetter && hasNumber;
+}
+
+
+const validateForm =() => {
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let age = document.getElementById("age").value;
+    let male = document.getElementById("male");
+    let female = document.getElementById("female");
+
+    if(username.length < 10) {
+        alert("Oh no, you need to insert your username more than 10 character!"); return;
+    }
+
+    if(!email.endsWith(".com")){
+        alert("Oh no, your email must end with .com!"); return;
+    }
+
+    if(!isAlphanumeric (password)){
+        alert ("Oh no, your password must be alphanumeric!"); return;
+    }
+
+    if (age < 10) {
+    alert("You need to be more than 10 years old to register!"); return;
+    }
+
+    if(!male.checked && !female.checked) {
+        alert("Oh no, you must pick a gender!"); return;
+    }
+
+    alert("Your Registration is Complete!")
+
+};
